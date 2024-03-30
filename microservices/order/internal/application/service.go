288ff25f3ac51132268ecb5c5ef101f720914aa1
Arine <peppydays@gmail.com>
2024-03-30 16:08:50 +0900

    Enhanced error handling

--- microservices/order/internal/application/service.go
+++ microservices/order/internal/application/service.go
@@ -1,7 +1,12 @@
 package application
 
 import (
+	"strings"
+
 	"github.com/peppydays/grpc-in-go-service/microservices/order/internal/domain"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type OrderService struct {
@@ -24,7 +29,27 @@ func (s OrderService) PlaceOrder(order domain.Order) (domain.Order, error) {
 
 	err = s.paymentGateway.Charge(&order)
 	if err != nil {
-		return domain.Order{}, err
+		orderStatus := status.New(codes.InvalidArgument, "order creation failed")
+
+		badReq := &errdetails.BadRequest{}
+		st := status.Convert(err)
+		var allErrors []string
+		for _, detail := range st.Details() {
+			switch t := detail.(type) {
+			case *errdetails.BadRequest:
+				for _, violation := range t.GetFieldViolations() {
+					allErrors = append(allErrors, violation.Description)
+				}
+			}
+		}
+		fieldErr := &errdetails.BadRequest_FieldViolation{
+			Field:       "payment",
+			Description: strings.Join(allErrors, "\n"),
+		}
+		badReq.FieldViolations = append(badReq.FieldViolations, fieldErr)
+
+		statusWithDetails, _ := orderStatus.WithDetails(badReq)
+		return domain.Order{}, statusWithDetails.Err()
 	}
 
 	return order, nil
